c81812f2b162f9590063c4a8894414867d3245b0
0498drew <mwaseandrew98@gmail.com>
2022-10-28 11:52:19 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """The Base ClassI"""
 
-import datetime
+from datetime import datetime
 import uuid
 import models
 
@@ -25,22 +25,21 @@ class BaseModel:
         """
 
         if kwargs:
-            for kwargs, val in kwargs.item():
-                if kwargs in ('created_at', 'updated_at'):
+            for k, val in kwargs.item():
+                if k == 'created_at' or k == 'updated_at':
                     val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
 
                 elif kwargs == '__class__':
                     continue
-                setattr(self, kwargs, val)
+                setattr(self, k, val)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """print [<class name>] (<self.id>) <self.__dict__>"""
-        return "[{}], ({}), {}".formart(
+        """print [<class name>] (<self.id>) <self.__dict__>")"""
+        return '[{}] ({}) {}'.format(
                 self.__class__.__name__, self.id, self.__dict__
                 )
 
@@ -53,9 +52,8 @@ class BaseModel:
 
     def to_dict(self):
         """diplaying class information"""
-        class_info = self.__dict__.copy()
-        class_info['__class__'] = self.__class__.__name__
-        class_info['created_at'] = self.created_at.isoformat()
-        class_info['updated_at'] = self.updated_at.isoformat()
-
+        dict_info = self.__dict__.copy()
+        dict_info['__class__'] = self.__class__.__name__
+        dict_info['created_at'] = self.created_at.isoformat()
+        dict_info['updated_at'] = self.updated_at.isoformat()
         return class_info
