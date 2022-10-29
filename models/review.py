14f7bca761810b352bd173055a04f6542a300025
0498drew <mwaseandrew98@gmail.com>
2022-10-29 14:32:28 -0700

    airbnb

---
+++ models/review.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+class Review
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    class attributes
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
