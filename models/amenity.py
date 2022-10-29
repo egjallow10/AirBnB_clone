8c069c605f2c0ba90b2748c0c6f58e20e8d2f3f3
0498drew <mwaseandrew98@gmail.com>
2022-10-29 08:49:44 -0700

    airbnb

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel)
+"""
+amenitry class attribute with empty strings
+"""
+name = ""
---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+from models.base_model import BaseModel
+
+
+class City(BaseModel)
+"""
+city class attribute with empty strings
+"""
+state_id = ""
+name = ""
